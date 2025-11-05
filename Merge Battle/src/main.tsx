e7e207d7348181968b7e32d88075bb0e16424033
s45357507-debug <s45357507@gmail.com>
2025-11-05 19:15:35 +0500

    Update main.tsx

--- Merge Battle/src/main.tsx
+++ Merge Battle/src/main.tsx
@@ -1,7 +1,10 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { AudioProvider } from "./context/AudioContext";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(
+  <AudioProvider>
+    <App />
+  </AudioProvider>
+);
